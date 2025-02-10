397703f1570bdb350747bf282531de06c5d6e18b
AliShokoohi23 <alibest3@yahoo.com>
2025-02-10 23:37:55 +0330

    update techstack

--- src/components/About/Techstack.tsx
+++ src/components/About/Techstack.tsx
@@ -7,17 +7,25 @@ import {
   DiGit,
 } from "react-icons/di";
 import {
-  SiCsharp,
   SiDotnet,
   SiSqlite,
   SiTypescript,
+  SiArduino,
+  SiHtml5,
+  SiCss3,
+  SiDocker,
+  SiAdobephotoshop,
+  SiProteus
 } from "react-icons/si";
+import { BiLogoPostgresql } from "react-icons/bi";
+
+import { TbBrandCSharp } from "react-icons/tb";
 
 function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
-        <SiCsharp />
+        <TbBrandCSharp />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiDotnet />
@@ -42,6 +50,27 @@ function Techstack() {
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiSqlite />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiArduino />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <BiLogoPostgresql />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiHtml5 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiCss3  />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiDocker  />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiAdobephotoshop  />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiProteus  />
       </Col>
     </Row>
   );
